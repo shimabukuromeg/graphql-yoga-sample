93f6f9ca1b53b3a036459306e122a98d2b5d637e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-26 14:45:44 +0900

    add query and mutation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,13 @@
-
-
-import { createServer } from 'node:http'
+import { createServer } from 'http'
 import { createYoga } from 'graphql-yoga'
 import { schema } from './schema'
 
-const yoga = createYoga({ schema })
-const server = createServer(yoga)
+function main() {
+    const yoga = createYoga({ schema })
+    const server = createServer(yoga)
+    server.listen(4000, () => {
+        console.info('Server is running on http://localhost:4000/graphql')
+    })
+}
 
-// Start the server and you're done!
-server.listen(4000, () => {
-    console.info('Server is running on http://localhost:4000/graphql')
-})
+main()
--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -1,14 +1,75 @@
-import { createSchema } from 'graphql-yoga'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
-export const schema = createSchema({
-    typeDefs: /* GraphQL */ `
-    type Query {
-      hello: String
+const typeDefinitions = /* GraphQL */ `
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Mutation {
+    postLink(url: String!, description: String!): Link!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+`
+
+// 1
+type Link = {
+    id: string
+    url: string
+    description: string
+}
+
+// 2
+const links: Link[] = [
+    {
+        id: 'link-0',
+        url: 'https://graphql-yoga.com',
+        description: 'The easiest way of setting up a GraphQL server'
     }
-  `,
-    resolvers: {
-        Query: {
-            hello: () => 'world'
+    , {
+        id: 'link-1',
+        url: 'https://graphql-yoga.com',
+        description: 'The easiest way of setting up a GraphQL server'
+    }
+]
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        // 3
+        feed: () => links
+    },
+    Mutation: {
+        postLink: (parent: unknown, args: { description: string; url: string }) => {
+            // 1
+            let idCount = links.length
+
+            // 2
+            const link: Link = {
+                id: `link-${idCount}`,
+                description: args.description,
+                url: args.url
+            }
+
+            links.push(link)
+
+            return link
         }
+    },
+    // 4
+    Link: {
+        id: (parent: Link) => parent.id,
+        description: (parent: Link) => parent.description,
+        url: (parent: Link) => parent.url
     }
+}
+
+export const schema = makeExecutableSchema({
+    resolvers: [resolvers],
+    typeDefs: [typeDefinitions]
 })
