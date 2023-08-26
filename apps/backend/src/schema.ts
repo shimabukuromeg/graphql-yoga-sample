95a925a5e42453108d5d33a02afb2ce43c9f6bed
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-27 01:43:49 +0900

    comment追加時に指定したlinkのidが間違ってたらエラーにした

--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -1,5 +1,8 @@
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { GraphQLContext } from './context'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+import { GraphQLError } from 'graphql'
+
 
 const typeDefinitions = /* GraphQL */ `
   type Query {
@@ -39,6 +42,14 @@ type Comment = {
     linkId: string
 }
 
+const parseIntSafe = (value: string): number | null => {
+    if (/^(\d+)$/.test(value)) {
+        return parseInt(value, 10)
+    }
+    return null
+}
+
+
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
@@ -76,14 +87,30 @@ const resolvers = {
             args: { linkId: string; body: string },
             context: GraphQLContext
         ) {
-            const newComment = await context.prisma.comment.create({
-                data: {
-                    linkId: parseInt(args.linkId),
-                    body: args.body
-                }
-            })
+            const linkId = parseIntSafe(args.linkId)
+            if (linkId === null) {
+                return Promise.reject(
+                    new GraphQLError(`Cannot post comment on non-existing link with id '${args.linkId}'.`)
+                )
+            }
 
-            return newComment
+            const comment = await context.prisma.comment
+                .create({
+                    data: {
+                        body: args.body,
+                        linkId: parseInt(args.linkId)
+                    }
+                })
+                .catch((err: unknown) => {
+                    if (err instanceof PrismaClientKnownRequestError && err.code === 'P2003') {
+                        return Promise.reject(
+                            new GraphQLError(`Cannot post comment on non-existing link with id '${args.linkId}'.`)
+                        )
+                    }
+                    return Promise.reject(err)
+                })
+
+            return comment
         }
     },
     Link: {
