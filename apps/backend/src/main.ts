14f0b6ea6ef27329430c96afd66d499eb5834a5c
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-27 10:24:12 +0900

    add fastify

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,12 +3,63 @@ import { createYoga } from 'graphql-yoga'
 import { schema } from './schema'
 import { createContext } from './context'
 
+// fastify 導入した。
+// 参考: https://the-guild.dev/graphql/yoga-server/docs/integrations/integration-with-fastify
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+
 function main() {
-    const yoga = createYoga({ schema, context: createContext })
-    const server = createServer(yoga)
-    server.listen(4000, () => {
-        console.info('Server is running on http://localhost:4000/graphql')
+    // This is the fastify instance you have created
+    const app = fastify({ logger: true })
+
+    const yoga = createYoga<{
+        req: FastifyRequest
+        reply: FastifyReply
+    }>({
+        schema,
+        context: createContext,
+        // Integrate Fastify logger
+        logging: {
+            debug: (...args) => args.forEach(arg => app.log.debug(arg)),
+            info: (...args) => args.forEach(arg => app.log.info(arg)),
+            warn: (...args) => args.forEach(arg => app.log.warn(arg)),
+            error: (...args) => args.forEach(arg => app.log.error(arg))
+        }
+    })
+
+
+    app.route({
+        // Bind to the Yoga's endpoint to avoid rendering on any path
+        url: yoga.graphqlEndpoint,
+        method: ['GET', 'POST', 'OPTIONS'],
+        handler: async (req, reply) => {
+            // Second parameter adds Fastify's `req` and `reply` to the GraphQL Context
+            const response = await yoga.handleNodeRequest(req, {
+                req,
+                reply
+            })
+            response.headers.forEach((value, key) => {
+                reply.header(key, value)
+            })
+
+            reply.status(response.status)
+
+            reply.send(response.body)
+
+            return reply
+        }
+    })
+
+    console.info("starting http server")
+    app.listen({ port: 4000 }, (err, address) => {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+        console.info(`server listening on ${address}`)
     })
 }
 
 main()
+
+
+
