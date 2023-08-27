d56e6a9337b8e73c6acd737396974325dd13296e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-27 09:18:30 +0900

    add filter

--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -7,7 +7,7 @@ import { GraphQLError } from 'graphql'
 const typeDefinitions = /* GraphQL */ `
   type Query {
     info: String!
-    feed: [Link!]!
+    feed(filterNeedle: String): [Link!]!
     comment(id: ID!): Comment
     link(id: ID): Link
   }
@@ -53,8 +53,16 @@ const parseIntSafe = (value: string): number | null => {
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: (_parent: unknown, _args: {}, context: GraphQLContext) => {
-            return context.prisma.link.findMany()
+        feed: (_parent: unknown, args: { filterNeedle?: string }, context: GraphQLContext) => {
+            const where = args.filterNeedle
+                ? {
+                    OR: [
+                        { description: { contains: args.filterNeedle } },
+                        { url: { contains: args.filterNeedle } }
+                    ]
+                }
+                : {}
+            return context.prisma.link.findMany({ where })
         },
         async comment(parent: unknown, args: { id: string }, context: GraphQLContext) {
             const comment = await context.prisma.comment.findUnique({
