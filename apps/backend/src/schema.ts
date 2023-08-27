1c9c1b91eab12a49bc7b7a4073ce10b3712830ea
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-08-27 10:08:13 +0900

    Pagination Field Argument Sanitization

--- apps/backend/src/schema.ts
+++ apps/backend/src/schema.ts
@@ -64,6 +64,7 @@ type User = {
     comments: Comment[]
 }
 
+
 const parseIntSafe = (value: string): number | null => {
     if (/^(\d+)$/.test(value)) {
         return parseInt(value, 10)
@@ -71,6 +72,23 @@ const parseIntSafe = (value: string): number | null => {
     return null
 }
 
+const applyTakeConstraints = (params: { min: number; max: number; value: number }) => {
+    if (params.value < params.min || params.value > params.max) {
+        throw new GraphQLError(
+            `'take' argument value '${params.value}' is outside the valid range of '${params.min}' to '${params.max}'.`
+        )
+    }
+    return params.value
+}
+
+const applySkipConstraints = (params: { min: number; value: number }) => {
+    if (params.value < params.min) {
+        throw new GraphQLError(
+            `'skip' argument value '${params.value}' is outside the valid range of '${params.min}'.`
+        )
+    }
+    return params.value
+}
 
 const resolvers = {
     Query: {
@@ -78,6 +96,9 @@ const resolvers = {
         feed: (_parent: unknown,
             args: { filterNeedle?: string; skip?: number; take?: number },
             context: GraphQLContext) => {
+
+
+
             const where = args.filterNeedle
                 ? {
                     OR: [
@@ -86,7 +107,19 @@ const resolvers = {
                     ]
                 }
                 : {}
-            return context.prisma.link.findMany({ where, skip: args.skip, take: args.take })
+
+            const take = applyTakeConstraints({
+                min: 1,
+                max: 50,
+                value: args.take ?? 30
+            })
+
+            const skip = applySkipConstraints({
+                min: 0,
+                value: args.skip ?? 0
+            })
+
+            return context.prisma.link.findMany({ where, skip, take })
         },
         async comment(parent: unknown, args: { id: string }, context: GraphQLContext) {
             const comment = await context.prisma.comment.findUnique({
