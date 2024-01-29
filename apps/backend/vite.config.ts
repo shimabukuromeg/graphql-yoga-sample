ece7c31a348770cb5d8a41dc390079062d92521c
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2024-01-30 00:26:51 +0900

    vitest 導入

---
+++ apps/backend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+    test: {
+        globals: true,
+        globalSetup: "test/setup-global.ts",
+        setupFiles: ["test/setup.ts"],
+        coverage: {
+            provider: "v8",
+            all: true,
+        }
+    },
+    plugins: [tsconfigPaths()],
+})
