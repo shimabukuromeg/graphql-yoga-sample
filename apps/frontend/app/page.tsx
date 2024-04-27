df4a477b6579c76cff62114981f2623f624eb638
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2024-04-27 12:06:11 +0900

    fix: デザイン調整

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <div className='flex justify-center'>
-      <div className='flex flex-col md:gap-8 gap-2 md:p-20 px-2 pt-4 max-w-[900px]'>
+      <div className='flex flex-col md:gap-6 gap-2 md:p-20 px-2 pt-4 max-w-[900px]'>
         <div>
           <h1 className="text-2xl md:text-3xl font-bold text-textBlack pb-4">全域</h1>
           <p className='font-bold text-textBlack'>{data.meshis.length}件</p>
