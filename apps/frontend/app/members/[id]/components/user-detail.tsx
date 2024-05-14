e98d9887f19100f76ccf1992cc66b6a0ff7b3cb5
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2024-05-15 06:45:33 +0900

    pnpm format:write

--- apps/frontend/app/members/[id]/components/user-detail.tsx
+++ apps/frontend/app/members/[id]/components/user-detail.tsx
@@ -1,18 +1,17 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card"
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from '@/components/ui/button'
-import { FragmentType, graphql, useFragment } from '@/src/gql'
+import { Button } from "@/components/ui/button"
+import { FragmentType, graphql, useFragment } from "@/src/gql"
 import { Icons } from "@/components/ui/icons"
 import Link from "next/link"
-
 
 export const UserDetailFragment = graphql(`
   fragment User on User {
@@ -26,24 +25,23 @@ export const UserDetailFragment = graphql(`
 `)
 
 type Props = {
-    user: FragmentType<typeof UserDetailFragment>
+	user: FragmentType<typeof UserDetailFragment>
 }
 
 export const UserDetail = (props: Props) => {
-    const user = useFragment(UserDetailFragment, props.user)
-    return (<div className='flex flex-col gap-8 flex-wrap justify-center md:justify-start'>
-        <div className='flex flex-row gap-3 items-center pb-2'>
-            <Avatar className="h-14 w-14">
-                <AvatarImage
-                    className=""
-                    src={user.iconImageURL ?? ""}
-                />
-                <AvatarFallback>{user.displayName.split('')[0]}</AvatarFallback>
-            </Avatar>
-            <CardTitle>{user.displayName}</CardTitle>
-        </div>
-        <CardDescription className="line-clamp-3 h-[40px]">
-            {user.description}
-        </CardDescription>
-    </div>)
+	const user = useFragment(UserDetailFragment, props.user)
+	return (
+		<div className="flex flex-col gap-8 flex-wrap justify-center md:justify-start">
+			<div className="flex flex-row gap-3 items-center pb-2">
+				<Avatar className="h-14 w-14">
+					<AvatarImage className="" src={user.iconImageURL ?? ""} />
+					<AvatarFallback>{user.displayName.split("")[0]}</AvatarFallback>
+				</Avatar>
+				<CardTitle>{user.displayName}</CardTitle>
+			</div>
+			<CardDescription className="line-clamp-3 h-[40px]">
+				{user.description}
+			</CardDescription>
+		</div>
+	)
 }
--- apps/frontend/app/members/[id]/page.tsx
+++ apps/frontend/app/members/[id]/page.tsx
@@ -1,49 +1,51 @@
-import { FragmentType, graphql, useFragment } from '@/src/gql'
-import { VariablesOf } from '@graphql-typed-document-node/core'
-import { GraphQLClient } from 'graphql-request'
-import { cache } from 'react'
-import { UserCard, UserCardFragment } from '../components/user-card'
+import { FragmentType, graphql, useFragment } from "@/src/gql"
+import { VariablesOf } from "@graphql-typed-document-node/core"
+import { GraphQLClient } from "graphql-request"
+import { cache } from "react"
+import { UserCard, UserCardFragment } from "../components/user-card"
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card"
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/ui/icons"
 import Link from "next/link"
-import { UserDetail } from './components/user-detail'
+import { UserDetail } from "./components/user-detail"
 
 type Props = {
-    params: {
-        id: string
-    }
-    searchParams?: {
-        [key: string]: string | string[] | undefined
-    }
+	params: {
+		id: string
+	}
+	searchParams?: {
+		[key: string]: string | string[] | undefined
+	}
 }
 export default async function Page({ params, searchParams }: Props) {
+	// 動作確認用
+	const x =
+		searchParams != null && typeof searchParams["createdAt"] === "string"
+			? "a"
+			: "b"
+	console.log("x", x)
 
-    // 動作確認用
-    const x = searchParams != null && typeof searchParams["createdAt"] === "string" ? "a" : "b"
-    console.log("x", x)
+	const { user } = await fetchUser({ id: params.id })
 
-    const { user } = await fetchUser({ id: params.id })
-
-    return (
-        <div className='flex flex-col p-8 md:p-20 gap-8'>
-            <h1 className="text-3xl font-bold">詳細ページ</h1>
-            <UserDetail user={user} />
-            {/* 戻る */}
-            <Link href={`/members`}>
-                <Button>戻る</Button>
-            </Link>
-        </div>
-    )
+	return (
+		<div className="flex flex-col p-8 md:p-20 gap-8">
+			<h1 className="text-3xl font-bold">詳細ページ</h1>
+			<UserDetail user={user} />
+			{/* 戻る */}
+			<Link href={`/members`}>
+				<Button>戻る</Button>
+			</Link>
+		</div>
+	)
 }
 
 const UserQuery = graphql(/* GraphQL */ `
@@ -54,32 +56,30 @@ const UserQuery = graphql(/* GraphQL */ `
     }
   `)
 
-const fetchUser = async (
-    input: VariablesOf<typeof UserQuery>
-) => {
-    const backendEndpoint = process.env.BACKEND_ENDPOINT ?? 'http://localhost:4000/graphql'
+const fetchUser = async (input: VariablesOf<typeof UserQuery>) => {
+	const backendEndpoint =
+		process.env.BACKEND_ENDPOINT ?? "http://localhost:4000/graphql"
 
-    const client = new GraphQLClient(backendEndpoint, {
-        fetch: cache(async (url: any, params: any) =>
-            fetch(url, { ...params, next: { revalidate: 60 } })
-        ),
-    });
-    const data = await client.request(UserQuery, input)
-    return data
+	const client = new GraphQLClient(backendEndpoint, {
+		fetch: cache(async (url: any, params: any) =>
+			fetch(url, { ...params, next: { revalidate: 60 } }),
+		),
+	})
+	const data = await client.request(UserQuery, input)
+	return data
 }
 
-const fetchUsers = async (
-    input: VariablesOf<typeof UsersQuery>
-) => {
-    const backendEndpoint = process.env.BACKEND_ENDPOINT ?? 'http://localhost:4000/graphql'
+const fetchUsers = async (input: VariablesOf<typeof UsersQuery>) => {
+	const backendEndpoint =
+		process.env.BACKEND_ENDPOINT ?? "http://localhost:4000/graphql"
 
-    const client = new GraphQLClient(backendEndpoint, {
-        fetch: cache(async (url: any, params: any) =>
-            fetch(url, { ...params, next: { revalidate: 60 } })
-        ),
-    });
-    const data = await client.request(UsersQuery, input)
-    return data
+	const client = new GraphQLClient(backendEndpoint, {
+		fetch: cache(async (url: any, params: any) =>
+			fetch(url, { ...params, next: { revalidate: 60 } }),
+		),
+	})
+	const data = await client.request(UsersQuery, input)
+	return data
 }
 
 const UsersQuery = graphql(/* GraphQL */ `
@@ -92,10 +92,9 @@ const UsersQuery = graphql(/* GraphQL */ `
   `)
 
 export async function generateStaticParams() {
-    const data = await fetchUsers({})
+	const data = await fetchUsers({})
 
-    return data.users.map((user) => ({
-        id: user.id,
-    }))
+	return data.users.map((user) => ({
+		id: user.id,
+	}))
 }
-
