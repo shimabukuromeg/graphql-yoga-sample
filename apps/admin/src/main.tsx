ca8cb94b9e51828ccb8223420c48387d1d4d99ed
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2025-03-31 21:11:15 +0900

    chore: adminアプリケーションを削除し、関連する設定ファイルや依存関係を整理。フロントエンドのワークスペース設定を更新。

--- apps/admin/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Routes } from '@generouted/react-router'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-)
