623ef8d3983023e690a520f07f3ce3b83f653785
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2025-05-06 10:57:50 +0900

    chore: PostgreSQLのイメージをPGroongaに変更し、データベースの初期化時にPGroonga拡張とインデックスを作成する処理を追加

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -6,6 +6,15 @@ const prisma = new PrismaClient()
 
 // 3
 async function main() {
+  // PGroonga拡張を有効化する
+  console.log('PGroonga拡張を有効化しています...')
+  try {
+    await prisma.$executeRawUnsafe('CREATE EXTENSION IF NOT EXISTS pgroonga;')
+    console.log('PGroonga拡張の有効化が完了しました')
+  } catch (error) {
+    console.error('PGroonga拡張の有効化中にエラーが発生しました:', error)
+  }
+
   await prisma.user.createMany({
     data: [
       {
@@ -37,6 +46,22 @@ async function main() {
       },
     ],
   })
+
+  // 他のテーブルデータ追加処理が終わった後にインデックスを作成
+  try {
+    // PGroongaインデックスを作成
+    console.log('PGroongaインデックスを作成しています...')
+    await prisma.$executeRawUnsafe(`
+      CREATE INDEX IF NOT EXISTS ix_meshis_title_store_name
+      ON meshis
+      USING pgroonga (
+        (title || ' ' || store_name)
+      );
+    `)
+    console.log('PGroongaインデックスの作成が完了しました')
+  } catch (error) {
+    console.error('PGroongaインデックスの作成中にエラーが発生しました:', error)
+  }
 }
 
 // 4
